d3d84bfbdbf05dec42e27b2c396e6fc2993e143f
AnastasiaCherniakh <anastayshablake@gmail.com>
2025-05-23 23:59:43 +0200

    improve error handling for missing API key

--- netlify/functions/getWeather.mjs
+++ netlify/functions/getWeather.mjs
@@ -5,6 +5,14 @@ export async function handler(event){
     try{
         const apiKey = process.env.WEATHER_API_KEY;
 
+        if (!apiKey) {
+            console.error("API key is missing!");
+            return {
+                statusCode: 500,
+                body: JSON.stringify({ error: "Missing WEATHER_API_KEY" })
+            };
+        }
+
         if(!event.body){
             return {
                 statusCode: 400,
@@ -12,11 +20,13 @@ export async function handler(event){
             };
         }
 
+        console.log("EVENT BODY:", event.body);
+        console.log("API KEY PRESENT:", !!apiKey);
+
         const { city } = JSON.parse(event.body);
 
         const response = await fetch(`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`);
         const data = await response.json();
-        console.log("EVENT BODY:", event.body);
     
         return {
             statusCode: "200",
